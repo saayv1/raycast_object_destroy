768b3cb4a90c734c9ab6a32b96e993b275b7f734
Vyaas <saayvsaayv@gmail.com>
2018-08-20 22:01:32 -0500

    first commit

---
+++ Assets/Raycast.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Raycast : MonoBehaviour {
+
+	public LineRenderer laserLineRenderer;
+	public float laserWidth = 0.1f;
+	public float laserMaxLength = 5f;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+		RaycastHit hit;
+		// Does the ray intersect any objects excluding the player layer
+		if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity))
+		{
+			Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
+			Debug.Log("Did Hit");
+			GameObject hitObject =  hit.transform.gameObject;
+			if (hitObject.tag == "bad") {
+
+				Destroy (hitObject);
+			}
+		}
+		else
+		{
+			Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 1000, Color.white);
+			Debug.Log("Did not Hit");
+
+		}
+	}
+
+
+}
